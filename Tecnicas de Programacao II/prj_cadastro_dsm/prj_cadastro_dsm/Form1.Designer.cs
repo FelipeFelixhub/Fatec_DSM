5d785cad49e1e65f8692b659a3d791c1aaf4d187
Felipe Felix <felipe.dfelix@gmail.com>
2023-10-24 21:23:37 -0300

    Projeto Crud com c#
    Adicionado funcao save e search

---
+++ Tecnicas de Programacao II/prj_cadastro_dsm/prj_cadastro_dsm/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace prj_cadastro_dsm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_cpf = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btn_salvar = new System.Windows.Forms.ToolStripButton();
+            this.btn_atualizar = new System.Windows.Forms.ToolStripButton();
+            this.btn_consultar = new System.Windows.Forms.ToolStripButton();
+            this.btn_deletar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_cpf
+            // 
+            this.txt_cpf.Location = new System.Drawing.Point(12, 57);
+            this.txt_cpf.Mask = "999.999.999-99";
+            this.txt_cpf.Name = "txt_cpf";
+            this.txt_cpf.Size = new System.Drawing.Size(103, 20);
+            this.txt_cpf.TabIndex = 0;
+            this.txt_cpf.Leave += new System.EventHandler(this.txt_cpf_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF DO CLIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOME DO CLIENTE";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(12, 116);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(369, 20);
+            this.txt_nome.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btn_salvar,
+            this.btn_atualizar,
+            this.btn_consultar,
+            this.btn_deletar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(507, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_salvar.Image = ((System.Drawing.Image)(resources.GetObject("btn_salvar.Image")));
+            this.btn_salvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(23, 22);
+            this.btn_salvar.Text = "toolStripButton1";
+            this.btn_salvar.ToolTipText = "GRAVAR";
+            this.btn_salvar.Click += new System.EventHandler(this.btn_salvar_Click);
+            // 
+            // btn_atualizar
+            // 
+            this.btn_atualizar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_atualizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_atualizar.Image")));
+            this.btn_atualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_atualizar.Name = "btn_atualizar";
+            this.btn_atualizar.Size = new System.Drawing.Size(23, 22);
+            this.btn_atualizar.Text = "toolStripButton2";
+            this.btn_atualizar.ToolTipText = "ATUALIZAR";
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_consultar.Image = ((System.Drawing.Image)(resources.GetObject("btn_consultar.Image")));
+            this.btn_consultar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_consultar.Name = "btn_consultar";
+            this.btn_consultar.Size = new System.Drawing.Size(23, 22);
+            this.btn_consultar.Text = "toolStripButton3";
+            this.btn_consultar.ToolTipText = "CONSULTAR";
+            // 
+            // btn_deletar
+            // 
+            this.btn_deletar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_deletar.Image = ((System.Drawing.Image)(resources.GetObject("btn_deletar.Image")));
+            this.btn_deletar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_deletar.Name = "btn_deletar";
+            this.btn_deletar.Size = new System.Drawing.Size(23, 22);
+            this.btn_deletar.Text = "toolStripButton4";
+            this.btn_deletar.ToolTipText = "DELETAR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 555);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_cpf);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CADASTRO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txt_cpf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btn_salvar;
+        private System.Windows.Forms.ToolStripButton btn_atualizar;
+        private System.Windows.Forms.ToolStripButton btn_consultar;
+        private System.Windows.Forms.ToolStripButton btn_deletar;
+    }
+}
+
---
+++ Tecnicas de Programacao II/prj_cadastro_dsm/prj_cadastro_dsm/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace prj_cadastro_dsm
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao;
+        SqlCommand comando;
+        string SQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexao = new SqlConnection(@"Server=LAB5-12;Database=cadastro_dsm_p2;trusted_connection=yes");
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conexão Show!!", "AVISO", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar!!", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQL = "insert into tb_cadastro(cpf,nome) values (@CPF, @NOME)";
+                comando = new SqlCommand(SQL, conexao);
+                comando.Parameters.AddWithValue("@CPF", txt_cpf.Text);
+                comando.Parameters.AddWithValue("@NOME", txt_nome.Text);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Dados gravados com sucesso!!", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txt_cpf.Clear();
+                txt_nome.Clear();
+                txt_cpf.Focus();
+            }
+            catch 
+            {
+
+                MessageBox.Show("Erro ao gravar os dados!!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void txt_cpf_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                SQL = "SELECT TOP(1) * FROM tb_cadastro WHERE CPF = @CPF";
+                comando = new SqlCommand(SQL, conexao);
+
+                comando.Parameters.AddWithValue("@CPF", txt_cpf.Text);
+                var retorno = comando.ExecuteReader();
+                if(retorno.Read())
+                {
+                    txt_nome.Text = retorno.GetValue(2).ToString();
+                }
+              
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+        }
+    }
+}
